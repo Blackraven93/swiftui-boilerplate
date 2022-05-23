b02cd98edf512a41cc77a6dd098e346396015fda
Raven <61782746+Blackraven93@users.noreply.github.com>
2022-05-23 23:15:54 +0900

    Initial Commit

---
+++ boilerplate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  boilerplate
+//
+//  Created by apple developer academy on 2022/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ boilerplate/boilerplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  boilerplateApp.swift
+//  boilerplate
+//
+//  Created by apple developer academy on 2022/05/23.
+//
+
+import SwiftUI
+
+@main
+struct boilerplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
